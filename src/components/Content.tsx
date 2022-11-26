a95d6dc3ce876e7b5091382a0813c6b4d785ed61
Scienboy <scienboy87@gmail.com>
2022-11-26 22:38:08 +0900

    Update contents and paperbase

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -6,10 +6,7 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import Tooltip from '@mui/material/Tooltip';
-import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
-import RefreshIcon from '@mui/icons-material/Refresh';
 
 export default function Content() {
   return (
@@ -40,11 +37,6 @@ export default function Content() {
               <Button variant="contained" sx={{ mr: 1 }}>
                 Search
               </Button>
-              <Tooltip title="Reload">
-                <IconButton>
-                  <RefreshIcon color="inherit" sx={{ display: 'block' }} />
-                </IconButton>
-              </Tooltip>
             </Grid>
           </Grid>
         </Toolbar>
