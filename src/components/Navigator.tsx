beb89e0fb15234cd4063e222ff9b7b62e5f4fc56
Scienboy <scienboy87@gmail.com>
2022-11-26 22:14:41 +0900

    1st commit

---
+++ src/components/Navigator.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import Divider from '@mui/material/Divider';
+import Drawer, { DrawerProps } from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
+import PeopleIcon from '@mui/icons-material/People';
+import DnsRoundedIcon from '@mui/icons-material/DnsRounded';
+import PermMediaOutlinedIcon from '@mui/icons-material/PhotoSizeSelectActual';
+import PublicIcon from '@mui/icons-material/Public';
+import SettingsEthernetIcon from '@mui/icons-material/SettingsEthernet';
+import SettingsInputComponentIcon from '@mui/icons-material/SettingsInputComponent';
+import TimerIcon from '@mui/icons-material/Timer';
+import SettingsIcon from '@mui/icons-material/Settings';
+import PhonelinkSetupIcon from '@mui/icons-material/PhonelinkSetup';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+const item = {
+  py: 2,
+  px: 2,
+  color: 'rgba(255, 255, 255, 0.7)',
+};
+
+const itemCategory = {
+  boxShadow: '0 -1px 0 rgb(255,255,255,0.1) inset',
+  py: 1.5,
+  px: 3,
+};
+
+export default function Navigator(props: DrawerProps) {
+  const { ...other } = props;
+
+  return (
+    <Drawer variant="permanent" {...other}>
+      <List disablePadding>
+        <ListItem sx={{ ...item, ...itemCategory, fontSize: 22, color: '#fff' }}>
+          News Analytics
+        </ListItem>
+        <ListItem sx={{ p: 0 }}>
+          <ListItemButton sx={item}>
+            <ListItemIcon>
+              <ShowChartIcon />
+            </ListItemIcon>
+            <ListItemText>News Trends</ListItemText>
+          </ListItemButton>
+        </ListItem>
+        <ListItem sx={{ p: 0 }}>
+          <ListItemButton sx={item}>
+            <ListItemIcon>
+              <FavoriteIcon />
+            </ListItemIcon>
+            <ListItemText>Sentiment Trends</ListItemText>
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Drawer>
+  );
+}
