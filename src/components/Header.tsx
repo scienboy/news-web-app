bddae5b9289020d08b1dedb58b64cb9c10549c2a
Scienboy <scienboy87@gmail.com>
2022-11-27 00:31:39 +0900

    Set header as menu name which is clicked

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
+
+import {useLocation} from "react-router-dom"
+
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
-const lightColor = 'rgba(255, 255, 255, 0.7)';
+import { NEWS_TRENDS_PATH, SENTIMENT_TRENDS_PATH } from '../App';
+
 
 export default function Header() {
+  const {pathname} = useLocation();
+
+  function getTitle() {
+    switch (pathname) {
+      case NEWS_TRENDS_PATH:
+        return "News Trends";
+      case SENTIMENT_TRENDS_PATH:
+        return "Sentiment Trends";
+      default:
+        return "Unknown";
+    }
+  }
+
+
   return (
     <AppBar 
       component="div"  
@@ -16,7 +34,7 @@ export default function Header() {
     >
       <Toolbar>
         <Typography color="inherit" variant="h5" component="h1">
-          Authentication
+          {getTitle()}
         </Typography>
       </Toolbar>
     </AppBar>
