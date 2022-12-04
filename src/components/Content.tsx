4ba8d05f3b0548ac98a9b1452f7e59860320fe8a
Scienboy <scienboy87@gmail.com>
2022-12-04 12:40:10 +0900

    HTML get method update (add search keyword in URL)

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 
 import AppBar from '@mui/material/AppBar';
@@ -16,18 +17,34 @@ import NewsTrendChart from './NewsTrendChart';
 
 export default function Content() {
 
-  const [inputText, setInputText] = useState(""); 
+  // const [searchText, setSearchText] = useState(""); 
+  const navigate = useNavigate();
+  const { pathname, search} = useLocation();
+
+  const params = new URLSearchParams(search); // URLSearchParams는 브라우저에서 제공하는 API. 파라미터를 파싱할 수 있음
+  const searchText = params.get("search") || ""; // 아니면 문자열 ""을 넣어라
+
+  const [inputText, setInputText] = useState(searchText); 
   // 리액트 개발 시 state variable과 그냥 variable의 차이점을 잘 이해해야 함.
   // state variable: 리액트가 관리하는 변수. 그래서 state variable을 변경하면 리액트가 다시 그림.
   // 그냥 variable: 리액트가 관리하지 않는 변수. 그래서 그냥 variable을 변경해도 리액트가 다시 그리지 않음.
   // inputText("hello"); // 이렇게 하면 리액트가 다시 그리지 않음. 그래서 state variable을 변경해야 함.
   // setInputText("hello"); // 이렇게 함수 호출 통해 값을 바꿔야 함
 
-  const [searchText, setSearchText] = useState(""); 
+
+  // console.log("pathname", pathname);
+  // console.log("search", search);
 
   function onSearch() {
-    console.log('inputText', inputText);
-    setSearchText(inputText);
+    // console.log('inputText', inputText);
+    // setSearchText(inputText);
+
+    // search 버튼을 누르면 url이 업데이트 됨
+    let url = pathname;
+    if (inputText) {
+      url += `?search=${inputText}`;
+    }
+    navigate(url); 
   }
 
 
@@ -77,6 +94,7 @@ export default function Content() {
         </Toolbar>
       </AppBar>
       <NewsTrendChart search={searchText}/>
+      {/* <NewsTrendChart/> */}
     </Paper>
   );
 }
