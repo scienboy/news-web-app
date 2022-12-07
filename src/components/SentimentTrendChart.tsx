92c498c7dba0d40a6f0dcd2ca0f3510b8ce94420
Scienboy <scienboy87@gmail.com>
2022-12-07 19:22:07 +0900

    Add toggle button

--- src/components/SentimentTrendChart.tsx
+++ src/components/SentimentTrendChart.tsx
@@ -6,6 +6,9 @@ import Box from '@mui/material/Box';
 import { useGetSentimentTrendsQuery } from "../app/newsApi";
 import { trace } from 'console';
 
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
 // NewsTrendChart가 받는 property가 무엇무엇인지 여기서 선언해 주고, 얘를 넘긴다.
 interface IProps {
     search: string;
@@ -16,6 +19,8 @@ interface IProps {
 export default function SentimentTrendChart({search}: IProps) { // 위에서 선언된 값을 여기서 받아서 사용함
     // const {data, isLoading} = useGetNewsTrendsQuery({search: ""}); // search를 받아라!
     const {data, isLoading} = useGetSentimentTrendsQuery({search}); // search를 받아라!
+    const [alignment, setAlignment] = React.useState('web');
+
 
     // 로딩중이거나 데이터가 없을 때 로딩중 아이콘(동그라미 도는 것) 보이도록 하기
     if (isLoading || !data) { 
@@ -50,6 +55,7 @@ export default function SentimentTrendChart({search}: IProps) { // 위에서 선
     //     mode: "lines+markers",
     // }
 
+
     const traces: Plotly.Data[] = ["positive", "neutral", "negative"].map(
         (x) => ({
             x: data.trends.map((el) => el.date),
@@ -59,13 +65,38 @@ export default function SentimentTrendChart({search}: IProps) { // 위에서 선
             name: x
         }),
     );
+    
+
+    const handleChange = (
+      event: React.MouseEvent<HTMLElement>,
+      newAlignment: string,
+    ) => {
+      setAlignment(newAlignment);
+    };
 
     return (
-        // Plot은 항상 목록을 받음. 그러므로 대괄호 안에 넣어줘야 함 e.g. [trace]
-        <Plot
-            data={traces}
-            layout={ {autosize: true}}
-            style={{width: "100%"}}
-        />
+        // pt: 3은 padding-top: 3
+        <Box sx={{ display: "grid", pt: 3}}> 
+          <ToggleButtonGroup
+                color="primary"
+                value={alignment}
+                exclusive
+                onChange={handleChange}
+                aria-label="Platform"
+                sx={{
+                    mx: "auto", // margin-x
+                }}
+                >
+                <ToggleButton value="doc-count">Doc Count</ToggleButton>
+                <ToggleButton value="score">Sentiment Score</ToggleButton>
+            </ToggleButtonGroup>
+                
+            {/* // Plot은 항상 목록을 받음. 그러므로 대괄호 안에 넣어줘야 함 e.g. [trace] */}
+            <Plot
+                data={traces}
+                layout={ {autosize: true}}
+                style={{width: "100%"}}
+            />
+        </Box>
     );
 }
